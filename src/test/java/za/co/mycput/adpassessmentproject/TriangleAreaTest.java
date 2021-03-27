093a1e1b8e29363944607e436f3cb9115017e411
michel muembo <michelmuemboilunga@gmail.com>
2021-03-28 01:13:46 +0200

    Project StartUp Structure First Commit

---
+++ src/test/java/za/co/mycput/adpassessmentproject/TriangleAreaTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.mycput.adpassessmentproject;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MICHEL MUEMBO ILUNGA ( Student Number: 218303335)
+ * 
+ * This is The TDD class
+ * 
+ */
+public class TriangleAreaTest {
+    
+    public TriangleAreaTest() {
+    }
+    
+  
+    @Before
+    public void setUp() {
+    }
+    
+   
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
